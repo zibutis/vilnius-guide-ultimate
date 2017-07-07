91ad27dd81f903d2f2cd40dcc05d0b600f3d17d4
kristijonas <kristijonas.zibutis@gmail.com>
2017-07-08 00:32:30 +0300

    first commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,28 @@
+class PagesController < ApplicationController
+
+
+  def ebook
+  end
+
+  def what_to_see_in_vilnius
+  end
+
+  def where_to_eat_in_vilnius
+  end
+
+  def where_to_party_in_vilnius
+  end
+
+  def where_to_stay_in_vilnius
+  end
+
+  def what_to_listen_in_vilnius
+  end
+
+  def fun_facts_about_Lithuania
+  end
+
+  def successful_ebook_purchase
+  end
+
+end
