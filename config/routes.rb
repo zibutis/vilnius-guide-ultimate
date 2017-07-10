e281e801b56660eeb5f9a2540d6f9a368bc1e686
kristijonas <kristijonas.zibutis@gmail.com>
2017-07-10 15:06:47 +0300

    editorial fixes and google analytics

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
 
   root to: 'pages#where_to_eat_in_vilnius'
 
-get "what_to_see_in_vilnius",       to: 'pages#what_to_see_in_vilnius'
-get "where_to_eat_in_vilnius",      to: 'pages#where_to_eat_in_vilnius'
-get "where_to_party_in_vilnius",    to: 'pages#where_to_party_in_vilnius'
-get "where_to_stay_in_vilnius",     to: 'pages#where_to_stay_in_vilnius'
-get "what_to_listen_in_vilnius",    to: 'pages#what_to_listen_in_vilnius'
-get "fun_facts_about_Lithuania",    to: 'pages#fun_facts_about_Lithuania'
+get "what-to-see-in-vilnius",       to: 'pages#what_to_see_in_vilnius'
+get "where-to-eat-in-vilnius",      to: 'pages#where_to_eat_in_vilnius'
+get "where-to-party-in-vilnius",    to: 'pages#where_to_party_in_vilnius'
+get "where-to-stay-in-vilnius",     to: 'pages#where_to_stay_in_vilnius'
+get "what-to-listen-in-vilnius",    to: 'pages#what_to_listen_in_vilnius'
+get "fun-facts-about-Lithuania",    to: 'pages#fun_facts_about_Lithuania'
 get "successful_ebook_purchase",    to: 'pages#successful_ebook_purchase'
 get "ebook",                        to: 'pages#ebook'
 
